f26ca7c350ceeaad04f7e74937495cc82bbabcf3
VanisaIndriyani <vanisaindriyani30@gmail.com>
2026-01-22 12:23:10 +0700

    Update db credentials for hosting

--- db_connect.php
+++ db_connect.php
@@ -1,8 +1,18 @@
 <?php
 $servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "run_for_peace";
+
+// Semak adakah kita berada di localhost atau live server
+if ($_SERVER['HTTP_HOST'] == 'localhost' || $_SERVER['HTTP_HOST'] == '127.0.0.1') {
+    // Tetapan Localhost (Laragon/XAMPP)
+    $username = "root";
+    $password = "";
+    $dbname = "run_for_peace";
+} else {
+    // Tetapan Hosting (Sila ubah ini mengikut hosting anda)
+    $username = "bitubimy_izsaa";
+    $password = "jokiizsaa200504";
+    $dbname = "bitubimy_run";
+}
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
