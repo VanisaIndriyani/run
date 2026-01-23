75f2098b5e0ddf0ac5e11849f6e3cf9991e79a1e
VanisaIndriyani <vanisaindriyani30@gmail.com>
2026-01-23 11:20:29 +0700

    Add animations and hover effects to registration pages

--- pendaftaran_berkumpul.php
+++ pendaftaran_berkumpul.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Pendaftaran Berkelompok - Run For Peace 2026</title>
-    <link rel="stylesheet" href="style_poster.css?v=10014">
+    <link rel="stylesheet" href="style_poster.css?v=10015">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;600;800&family=Poppins:wght@400;600;700&family=Dancing+Script:wght@700&family=Bangers&display=swap" rel="stylesheet">
