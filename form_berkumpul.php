35c436a5660931fc13df1889ec88c5ed7a18c08f
VanisaIndriyani <vanisaindriyani30@gmail.com>
2026-01-29 14:05:18 +0700

    Bump CSS version to 10007 to fix caching issues on hosting

--- form_berkumpul.php
+++ form_berkumpul.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Pendaftaran Berkumpul - Run For Peace 2026</title>
-    <link rel="stylesheet" href="style_form.css?v=10005">
+    <link rel="stylesheet" href="style_form.css?v=10007">
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
 </head>
